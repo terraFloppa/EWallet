fd5c98a6d397eec61b53e86f2f0d3c36b153174f
terraFloppa <ars3n1yn1ght@gmail.com>
2026-06-18 23:50:18 +0300

    FIX: transaction edit screen

--- app/src/main/java/com/example/finance5/ui/screen/TransactionEditScreen.kt
+++ app/src/main/java/com/example/finance5/ui/screen/TransactionEditScreen.kt
@@ -1,12 +1,18 @@
 package com.example.finance5.ui.screen
 
 import androidx.collection.emptyLongSet
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.DatePickerState
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -18,7 +24,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
 import com.example.finance5.NavRoutes
@@ -27,6 +36,9 @@ import com.example.finance5.ui.viewmodel.CategoryViewModel
 import com.example.finance5.ui.viewmodel.TransactionViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
+import java.time.Instant
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -61,7 +73,23 @@ fun TransactionEditScreen(
     val amountValue = amountState.toDoubleOrNull() ?: 0.0
     val categoryValue = categoryState.toIntOrNull() ?: 0
 
-    Column {
+    // Форматирование даты в строку
+    val formattedDate = remember(selectedDate) {
+        if (selectedDate != null) {
+            val instant = Instant.ofEpochMilli(selectedDate!!)
+            val formatter = DateTimeFormatter
+                .ofPattern("dd.MM.yyyy")
+                .withZone(ZoneId.systemDefault())
+            formatter.format(instant)
+        } else {
+            "Дата не выбрана"
+        }
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize().padding(40.dp, 0.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
         Text("Измените транзакцию")
         // Сумма
         TextField(
@@ -72,22 +100,22 @@ fun TransactionEditScreen(
         )
         // Выбор категории
         SelectCategoryButton(categoryOptions, selectedCategory, isExpanded)
-        // Выбор даты
-        Button(
-            onClick = { showModal = true }
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        // Блок выбора и отображения даты
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
         ) {
-            if (selectedDate == null) {
-                Text("Введите дату")
+            TextButton(
+                onClick = { showModal = true }
+            ) {
+                Text("Выбрать дату")
             }
-            else {
-                Text(selectedDate.toString())
-            }
-        }
-        if (showModal) {
-            DatePickerModal(
-                onDateSelected = { selectedDate = it },
-                onDismiss = { showModal = false },
-                datePickerState
+            Text(
+                text = formattedDate,
+                style = MaterialTheme.typography.bodyMedium
             )
         }
         // Кнопки
@@ -100,6 +128,17 @@ fun TransactionEditScreen(
             // Кнопка удаления
             DeleteButton(amountValue, categoryValue, transactionViewModel, datePickerState, id,
                 navController, scope, snackbarHostState
+            )
+        }
+
+        if (showModal) {
+            DatePickerModal(
+                onDateSelected = {
+                    selectedDate = it
+                    showModal = false // Закрываем модалку после выбора
+                },
+                onDismiss = { showModal = false },
+                datePickerState = datePickerState
             )
         }
     }
